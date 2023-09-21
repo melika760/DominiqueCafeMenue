8bfdcad12c6b043c542fd4649383f723ab71a594
melika760 <72795928+melika760@users.noreply.github.com>
2023-09-21 13:44:21 +0330

    adding footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 import Header from "./Components/Header";
 import AboutUs from "./Components/AboutUs";
 import Reserve from "./Components/Reserve";
+import Footer from "./Components/Footer";
 function App() {
   return (
 <Fragment>
 <Header/>
 <AboutUs/>
 <Reserve/>
+<Footer/>
 </Fragment>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,20 @@
+import styles from "./Footer.module.css"
+export default function Footer(){
+    return(
+        <div className={styles.wraper} >
+<div>
+    <h3>Address</h3>
+    <p>19 S End Rd, London NW3 2PT</p>
+</div>
+<div>
+    <h3>Opening Hours</h3>
+    <p> Every day 9 am - 5pm</p>
+    <p> Monday closed.</p>
+</div>
+<div>
+    <h3>Contact Us</h3>
+    <p>Call Number: 02074319929</p>
+</div>
+        </div>
+    )
+}
