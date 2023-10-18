e255be3a18d58b78988323c0a3750482e7213e3d
melika760 <72795928+melika760@users.noreply.github.com>
2023-10-18 13:12:15 +0330

    responsive

--- src/Components/Reserve.js
+++ src/Components/Reserve.js
@@ -4,9 +4,9 @@ export default function Reserve(){
     return(<div className={styles.wraper}>
         <h3>Book a Table</h3>
         <form className={styles.form}>
-        <input type="date" placeholder="Date"/>
-        <input type="time"  placeholder="Date"/>
-        <input type="number" min={0}  placeholder="Number"/>
+        <input type="date" placeholder="Date" className={styles.num}/>
+        <input type="time"  placeholder="Date" className={styles.set}/>
+        <input type="number" min={0}  placeholder="Number" className={styles.num}/>
         <Button className={styles.btn}>Book</Button>
     </form></div>)
 }
